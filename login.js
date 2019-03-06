c2723159b518d832e193229de7b0cea27e4c977f
Diego <diegoturciostc@gmail.com>
2019-03-06 17:55:27 -0600

    fixing error'

--- login.js
+++ login.js
@@ -29,9 +29,16 @@ app.post('/auth', function(request, response) {
 	var password = request.body.password;
 	console.log(username);
 	console.log(password);
+
 	if (username && password) {
-		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
-			if (results.length > 0) {
+		var query = `SELECT * FROM accounts WHERE username = ${username} AND password = ${password}`;
+		console.log(query);		
+		//connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+		connection.query(query, function(error, results, fields) {
+			if (results == null){
+				response.send('Incorrect Username and/or Password!');
+			}			
+			else if (results.length > 0) {
 				request.session.loggedin = true;
 				request.session.username = username;
 				response.redirect('/home');
